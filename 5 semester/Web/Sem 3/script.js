d7d14038d260223a6ffa55fb21c98ab6dc3b8b75
ffrrwwrr <ffrrwwrr@gmail.com>
2021-09-22 10:22:10 +0300

    Web: add t-2

---
+++ 5 semester/Web/Sem 3/script.js
@@ -0,0 +1,5 @@
+function onclick1() {
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+    alert(`Спасибо за заполнение анкеты, ${name}!\nМы скинем Вам приглашение на почту ${email}.`)
+}

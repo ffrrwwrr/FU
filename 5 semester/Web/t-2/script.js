cb41bc6c810dc0f8190b09568df7280d05414a0b
ffrrwwrr <ffrrwwrr@gmail.com>
2021-09-23 02:29:22 +0300

    Web: Sem 3 done

--- 5 semester/Web/t-2/script.js
+++ 5 semester/Web/t-2/script.js
@@ -1,4 +1,4 @@
 function onclick1 () {
     const text = document.getElementById("textId").value
-    alert(text.match(/http:/g).length)
+    alert(text.match(/(^http:[\/][\/])([\w][\w][\w][\.]?)([a-zA-Z0-9]*)([\.])([a-z]*$)/gm).length)
 }

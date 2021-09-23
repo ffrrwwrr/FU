69849bbbbf78001822b30f358787d43804724572
ffrrwwrr <ffrrwwrr@gmail.com>
2021-09-23 20:23:13 +0300

    Web t-2 and t-1 and t-0 completed

--- 5 semester/Web/t-2/script.js
+++ 5 semester/Web/t-2/script.js
@@ -1,4 +1,4 @@
 function onclick1 () {
     const text = document.getElementById("textId").value
-    alert(text.match(/(^http:[\/][\/])([\w][\w][\w][\.]?)([a-zA-Z0-9]*)([\.])([a-z]*$)/gm).length)
+    alert(`Кол-во ссылок начинающихся на "http:" : ${text.match(/(^http:[\/][\/])([\w][\w][\w][\.]?)([a-zA-Z0-9]*)([\.])([a-z]*$)/gm).length}`)
 }

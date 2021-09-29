544a9ab562664a598bdcd86bf5b9c72d6bf1877e
ffrrwwrr <ffrrwwrr@gmail.com>
2021-09-29 17:33:17 +0300

    Add folder BuhIS, start t-3

--- 5 semester/Web/t-2/script.js
+++ 5 semester/Web/t-2/script.js
@@ -1,5 +1,10 @@
 function onclick1 () {
     const text = document.getElementById("textId").value
-    alert(`Кол-во ссылок начинающихся на "http:" : ${text.match(/(^http:[\/][\/])([\w][\w][\w][\.]?)([a-zA-Z0-9]*)([\.])([a-z]*$)/gm).length}`)
+<<<<<<< HEAD
+    alert(text.match(/http:/g).length)
 }
 
+=======
+    alert(`Кол-во ссылок начинающихся на "http:" : ${text.match(/(^http:[\/][\/])([\w][\w][\w][\.]?)([a-zA-Z0-9]*)([\.])([a-z]*$)/gm).length}`)
+}
+>>>>>>> 7276a42aea78f4fe37f2d42f43ec5a4b9d4666ac

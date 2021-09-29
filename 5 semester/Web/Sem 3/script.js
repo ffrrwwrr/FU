1ffee7bf172cb5c9f1f179fc1bda0405990c48ba
ffrrwwrr <ffrrwwrr@gmail.com>
2021-09-29 13:35:57 +0300

    t-2

--- 5 semester/Web/Sem 3/script.js
+++
@@ -1,5 +0,0 @@
-function onclick1() {
-    const name = document.getElementById("name").value
-    const email = document.getElementById("email").value
-    alert(`Спасибо за заполнение анкеты, ${name}!\nМы скинем Вам приглашение на почту ${email}.`)
-}
--- 5 semester/Web/t-2/script.js
+++ 5 semester/Web/t-2/script.js
@@ -1,10 +1,5 @@
 function onclick1 () {
     const text = document.getElementById("textId").value
-<<<<<<< HEAD
-    alert(text.match(/http:/g).length)
-}
-
-=======
     alert(`Кол-во ссылок начинающихся на "http:" : ${text.match(/(^http:[\/][\/])([\w][\w][\w][\.]?)([a-zA-Z0-9]*)([\.])([a-z]*$)/gm).length}`)
 }
->>>>>>> 7276a42aea78f4fe37f2d42f43ec5a4b9d4666ac
+
